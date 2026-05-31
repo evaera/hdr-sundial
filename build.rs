44403282924aa4987f34efef794ffbf46256189b
Eryn L. K. <e@eryn.io>
2026-05-30 19:03:21 -0700

    Fix windows metadata

--- build.rs
+++ build.rs
@@ -5,9 +5,10 @@ fn main() {
     {
         let mut res = winresource::WindowsResource::new();
         res.set_icon("assets/sundial.ico");
+        res.set("FileDescription", "HDR Sundial");
         res.set("ProductName", "HDR Sundial");
         res.set(
-            "FileDescription",
+            "Comments",
             "Sets Windows SDR content brightness from the sun",
         );
         res.set("CompanyName", "evaera");
